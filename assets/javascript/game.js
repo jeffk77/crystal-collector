9446c73f9404e0191f6cda2ce632039792675e97
jeffk77 <jeff.kushner@me.com>
2018-06-14 16:48:48 -0400

    Commit Further Files, Java Starting to Work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,71 +1,70 @@
-var yourScore = document.getElementById('yourScore');
-//var yourScore = getElementById(red, blue, yellow, green.onclick(math.sum));
-yourScore.innerHTML = 0; //
-var targetScore = document.getElementById('targetScore');
-targetScore.innerHTML = getRandomValue(10, 4);
+var yourScore = document.getElementById('usernumWindow');
+    yourScore.innerHTML = 0;
+
+var targetScore = document.getElementById('compnumWindow');
+    targetScore.innerHTML = getRandomValue(120, 19);
+
+var targetScore = document.getElementById('diamondvalue1');
+    targetScore.innerHTML = getRandomValue(12, 1);
+
+var targetScore = document.getElementById('diamondvalue2');
+    targetScore.innerHTML = getRandomValue(12, 1);
+
+var targetScore = document.getElementById('diamondvalue3');
+    targetScore.innerHTML = getRandomValue(12, 1);
+
+var targetScore = document.getElementById('diamondvalue4');
+    targetScore.innerHTML = getRandomValue(12, 1);
 
 function addToScore(val) {
-  var numberToAdd = parseInt(val);
-  var currentScore = parseInt(yourScore.innerHTML);
-  yourScore.innerHTML = numberToAdd + currentScore;
+    var numberToAdd = parseInt(val);
+    var currentScore = parseInt(yourScore.innerHTML);
+    yourScore.innerHTML = numberToAdd + currentScore;
 }
 
-var red = document.getElementById("red");
-//red.setAttribute("value", "5");
-console.log(red.value);
+var diamond1 = document.getElementById("diamond1");
+var diamond2 = document.getElementById("diamond2");
+var diamond3 = document.getElementById("diamond3");
+var diamond4 = document.getElementById("diamond4");
 
-var blue = document.getElementById("blue");
-//console.log(blue.value);
 
-var yellow = document.getElementById("yellow");
-//console.log(yellow.value);
 
-var green = document.getElementById("green");
-//console.log(green.value);
 
-// add an event listener to elements
-red.addEventListener('click', function() {
-  //this refers to red
-  addToScore(this.value);
-  checkPlayerScore();
+diamond1.addEventListener('click', function () {
+    addToScore(this.value);
+    checkPlayerScore();
 });
 
 
-blue.addEventListener('click', function() {
-  // this refers to blue
-  addToScore(this.value);
-  checkPlayerScore();
+diamond2.addEventListener('click', function () {
+    addToScore(this.value);
+    checkPlayerScore();
 });
 
-yellow.addEventListener('click', function() {
-  // this refers to blue
-  addToScore(this.value);
-  checkPlayerScore();
+diamond3.addEventListener('click', function () {
+    addToScore(this.value);
+    checkPlayerScore();
 });
 
-green.addEventListener('click', function() {
-  // this refers to blue
-  addToScore(this.value);
-  checkPlayerScore();
+diamond4.addEventListener('click', function () {
+    addToScore(this.value);
+    checkPlayerScore();
 });
-//var sum = parseInt(red.value) + parseInt(blue.value) + parseInt(yelow.value) + parseInt(green.value);
 
-// console.log (sum);
-function getRandomValue (a, b){
-  return Math.floor((Math.random() * a) + b);
+
+
+function getRandomValue(a, b) {
+    return Math.floor((Math.random() * a) + b);
 }
 
 function checkPlayerScore() {
-  //yourScore, targetScore (parseFloat() is for decimals)
-  var playerInt = parseInt(yourScore.innerHTML);
-  var targetInt = parseInt(targetScore.innerHTML);
-  if (playerInt === targetInt) {
-      alert('You win!');
+    var playerInt = parseInt(yourScore.innerHTML);
+    var targetInt = parseInt(targetScore.innerHTML);
+    if (playerInt === targetInt) {
+        alert('You win!');
         wins++;
-  } else if (playerInt > targetInt) {
-    alert('You lose!');
+    } else if (playerInt > targetInt) {
+        alert('You lose!');
         losses++;
-  } 
+    }
 }
-
-console.log(getRandomValue(10, 4));
