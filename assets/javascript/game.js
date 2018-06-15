40d7c272ca11bfd8022b02569ac5fb6564d7b538
Jeff Kushner <37851930+jeffk77@users.noreply.github.com>
2018-06-15 00:10:31 -0400

    Adding the commenting.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
+
+    // Selects random number and puts it in the "compNum" div
     var Random = Math.floor(Math.random() * 101 + 19)
     $('#compNum').text(Random);
 
+    // Creates the variables for use later, with established zero values and array.
     var GeneratedNumbers = []
     var wins = 0;
     var losses = 0;
     var total = 0;
+
+    // Provides values for Win/Loss tracking divs.
     $('#numberWins').text(wins);
     $('#numberLosses').text(losses);
 
+    // Random value generator for the diamond buttons.
     function fourRandomNumbers() {
         for (var i = 0; i < 4; i++) {
             var num = Math.floor(Math.random() * 11 + 1);
@@ -16,8 +22,10 @@ $(document).ready(function () {
         }
     }
 
+    // Creates random number reset.
     fourRandomNumbers();
 
+    // Reset function, to reset the game after each round.
     function reset() {
         Random = Math.floor(Math.random() * 101 + 19);
         $('#compNum').text(Random);
@@ -27,6 +35,7 @@ $(document).ready(function () {
         $('#userNum').text(total);
     }
 
+    // addWin Function, used to track when the user wins!
     function addWin() {
         alert("You Win!");
         wins++;
@@ -34,6 +43,7 @@ $(document).ready(function () {
         reset();
     }
 
+    // addLoss Function, used to track when the user loses!
     function addLoss() {
         alert("You Lose!");
         losses++;
@@ -41,6 +51,7 @@ $(document).ready(function () {
         reset();
     }
 
+    // The on-click response function.
     $('.diamondbutton').on('click', function() {
         var elem = $(this);
         var indx = elem.data('idx');
